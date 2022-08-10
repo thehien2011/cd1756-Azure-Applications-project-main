e84a0939c279bd539185f354b98d2c43fb3b2432
Nguyen Dinh Hien <thehien.nguyen00@gmail.com>
2022-08-10 23:23:50 +0700

    log

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -66,8 +66,10 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
+            app.logger.warning('Invalid username or password')
             flash('Invalid username or password')
             return redirect(url_for('login'))
+        app.logger.warning('login success')
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
